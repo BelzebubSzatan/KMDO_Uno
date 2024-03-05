c2e1a5ce5ca61b60f43716ce85aa7a78c6cff1c6
KacperMatlag <matlagkacper@gmail.com>
2024-03-05 12:11:32 +0100

    Basic backend setup

--- KMDO_Uno/KMDO_Uno/MainPage.xaml.cs
+++ KMDO_Uno/KMDO_Uno/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KMDO_Uno.models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -10,11 +11,41 @@ namespace KMDO_Uno
 {
     public partial class MainPage : ContentPage
     {
+        Deck deck = new Deck();
+        List<Card> playerCards= new List<Card>();
+        List<Card> computerCards= new List<Card>();
+        Card middleCard= null;
+        bool win=false;
+        bool playerAction = true;
         public MainPage()
         {
             InitializeComponent();
+            playerCards = deck.GetPlayerCards(3);
+            computerCards = deck.GetPlayerCards(3);
+            RenderPlayerCards();
+            SetMiddleCard(null);
         }
+        void SetMiddleCard(Card c)
+        {
 
+        }
+        void RenderPlayerCards()
+        {
+
+        }
+        private void PlayerCardClick(object sender, EventArgs e)
+        {
+
+        }
+        void WinCheck()
+        {
+
+        }
+        void SpecialCard()
+        {
+
+        }
+        void ComputerMove() { }
         private void Button_Clicked(object sender, EventArgs e)
         {
 
